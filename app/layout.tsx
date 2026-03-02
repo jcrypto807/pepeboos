66d46a587add7e50273870540afb3fb06e696bac
jcrypto807 <jcrypto807@gmail.com>
2026-03-03 04:27:49 +0530

    Add files via upload

---
+++ app/layout.tsx
@@ -0,0 +1,39 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import Web3Provider from "@/providers/web3-provider";
+import { Toaster } from "@/components/ui/sonner";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "PEPE BOSS: BEST CRYPTO PRESALE 2026",
+  description: "PEPE BOSS: BEST CRYPTO PRESALE 2026",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <Web3Provider>
+          {children}
+          <Toaster />
+        </Web3Provider>
+      </body>
+    </html>
+  );
+}
---
+++ hooks/use-price.tsx
@@ -0,0 +1,44 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+const usePrice = () => {
+  const initialData = null;
+
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["priceData"],
+    queryFn: async () => {
+      const result = await axios.get("https://coinfetcher-price-6efi.onrender.com/price");
+
+      return { ...result.data.price, USDT: 1, USDC: 1, BUSD: 1, IONX: 0.000000186 };
+    },
+    refetchInterval: 10000, // Refetch every 10 seconds
+  });
+
+  if (isLoading) return initialData;
+  if (error) return initialData;
+
+  return data || initialData;
+};
+
+export default usePrice;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
